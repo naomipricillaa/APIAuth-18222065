3991b9f0d68a930d18d329075bd6699a4ffde069
naomipricillaa <naaooommiii16@gmail.com>
2024-11-24 22:22:32 +0700

    debug

--- app/main.py
+++ app/main.py
@@ -10,17 +10,14 @@ app = FastAPI()
 # Register routes
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
-# Dynamically resolve the base directory
-BASE_DIR = Path(__file__).resolve().parent
-TEMPLATE_DIR = BASE_DIR / "templates"
-
-# Initialize templates
-templates = Jinja2Templates(directory=str(TEMPLATE_DIR.resolve()))
+templates = Jinja2Templates(directory="app/templates")  
 
 # Login route
 @app.get("/", response_class=HTMLResponse)
 def show_login_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+app.include_router(supabase_router)
 
 # Run the app (local development only)
 if __name__ == "__main__":
