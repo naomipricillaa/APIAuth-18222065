6c0c3b53cb781fae55a1022289d39ff04c217ed9
naomipricillaa <naaooommiii16@gmail.com>
2024-11-24 20:44:34 +0700

    debug

--- app/main.py
+++ app/main.py
@@ -1,28 +1,7 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from app.routes import auth
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Register routes
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
-
-# Add template directory
-templates = Jinja2Templates(directory="templates")
-
-# Home route
-@app.get("/", response_class=HTMLResponse)
-def show_home_page(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-# Login route
-@app.get("/login", response_class=HTMLResponse)
-def show_login_page(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-# Run the app (local development only)
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Vercel!"}
